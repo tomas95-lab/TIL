d85f0b4bcb9c05f3412968e7878cef5442fcf969
tomas ruiz <truiz050904@gmail.com>
2024-12-15 21:33:21 -0300

    Implementing the load images portfolio section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
     "use strict";
 
-    // Spinner
+
     var spinner = function () {
         setTimeout(function () {
             if ($('#spinner').length > 0) {
@@ -12,11 +12,11 @@
     spinner();
     
     
-    // Initiate the wowjs
+
     new WOW().init();
 
 
-    // Sticky Navbar
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.sticky-top').addClass('shadow-sm').css('top', '0px');
@@ -26,7 +26,7 @@
     });
     
     
-    // Back to top button
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
@@ -40,14 +40,14 @@
     });
 
 
-    // Facts counter
+
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
 
 
-    // Header carousel
+
     $(".header-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
@@ -62,7 +62,7 @@
     });
 
 
-    // Testimonials carousel
+
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -85,7 +85,7 @@
     });
 
 
-    // Portfolio isotope and filter
+
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
@@ -99,3 +99,61 @@
     
 })(jQuery);
 
+
+const portfolioContainer = document.querySelector(".projects");
+const totalImages = 35;
+const imagesPerLoad = 6;
+const loadMoreButton = document.getElementById('load-more');
+
+let currentImageIndex = 1; 
+
+function loadImages() {
+
+    const limit = Math.min(currentImageIndex + imagesPerLoad - 1, totalImages);
+
+
+    for (let i = currentImageIndex; i <= limit; i++) {
+        const projectDiv = document.createElement('div');
+    projectDiv.classList.add('col-lg-4', 'col-md-6', 'portfolio-item', 'first', 'wow', 'fadeInUp');
+    projectDiv.setAttribute('data-wow-delay', '0.1s');
+
+
+    projectDiv.innerHTML = `
+        <div class="rounded overflow-hidden m-2">
+            <div class="position-relative overflow-hidden">
+                <img class="img-fluid w-100 portfolio-img" src="img/portfolio/imagen (${i}).jpg" alt="">
+                <div class="portfolio-overlay">
+                    <a class="btn btn-square btn-outline-light mx-1" href="img/portfolio/imagen (${i}).jpg" data-lightbox="portfolio">
+                        <i class="fa fa-eye"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="border border-5 border-light border-top-0 p-4">
+                <p class="text-primary fw-medium mb-2">Carpintería General</p>
+                <h5 class="lh-base mb-0">Fabricación y Remodelación de Muebles de Madera</h5>
+            </div>
+        </div>
+
+    `;
+
+
+    portfolioContainer.appendChild(projectDiv);
+    }
+
+
+    currentImageIndex = limit + 1;
+
+
+    if (currentImageIndex > totalImages) {
+        loadMoreButton.style.display = 'none';
+    }
+}
+
+loadImages();
+
+loadMoreButton.addEventListener('click', loadImages);
+
+for (let i = 1; i <= totalImagenes; i++) {
+
+    
+}
